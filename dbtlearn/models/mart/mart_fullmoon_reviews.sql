97998afee37b6629e3200bd0166b2ec5b9bb6fb6
matotias <matotias@gmail.com>
2022-06-01 21:31:30 -0400

    feat(models): add mart_fullmoon_reviews table

---
+++ dbtlearn/models/mart/mart_fullmoon_reviews.sql
@@ -0,0 +1,22 @@
+{{
+    config(
+        materialized = 'table',
+    )
+}}
+with fct_reviews as (
+    select *
+    from {{ ref('fct_reviews') }}
+),
+full_moon_dates as (
+    select *
+    from {{ ref('seed_full_moon_dates') }}
+)
+select
+    fct_reviews.*,
+    case
+        when full_moon_dates.full_moon_date is null then 'not full moon'
+        else 'full moon'
+    end as is_full_moon
+from fct_reviews
+left join full_moon_dates
+    on TO_DATE(fct_reviews.review_date) = DATEADD(day, 1, full_moon_dates.full_moon_date)
